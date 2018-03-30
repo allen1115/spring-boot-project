694c29e9eaa47200a22d863a28c310dccb19a2b8
ashen054 <allen.g.shen@pwc.com>
2018-03-30 18:01:22 +0800

    login function

--- src/main/java/com/example/demo/controller/page/IndexController.java
+++ src/main/java/com/example/demo/controller/page/IndexController.java
@@ -1,18 +1,30 @@
 package com.example.demo.controller.page;
 
+import com.example.demo.entity.UserLogin;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class IndexController {
 
     @RequestMapping(value = "/")
-    public String index() {
+    public String index(HttpSession session) {
+        UserLogin user = (UserLogin) session.getAttribute("user");
+        if(user != null) {
+            return "/index";
+        }
         return "/login";
     }
 
-    @RequestMapping("/login")
-    public String login(){
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login(HttpSession session){
+        UserLogin user = (UserLogin) session.getAttribute("user");
+        if(user != null) {
+            return "/index";
+        }
         return "login";
     }
 
