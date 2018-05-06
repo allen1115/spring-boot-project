955be23973440d220e4a27d2b47e7477e5d0a29f
Sony <sony.i.cai@pwc.com>
2018-05-06 16:18:43 +0800

    dashboard

--- src/main/resources/static/js/dashBoard.js
+++ src/main/resources/static/js/dashBoard.js
@@ -1,5 +1,7 @@
 $(function () {
-    initChart();
+    initBarChart();
+    initPieChart();
+    initLineChart();
     initSelect();
     // init();
     initEmailBox();
@@ -9,7 +11,7 @@ $(function () {
 
 
     
-    function initChart() {
+    function initBarChart() {
         var ctx = document.getElementById("bar_chart").getContext('2d');
         myChart = new Chart(ctx, {
             type: 'bar',
@@ -45,6 +47,74 @@ $(function () {
                         'rgba(54, 162, 235, 1)',
                         'rgba(255, 206, 86, 1)',
                         'rgba(75, 192, 192, 1)',
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true,
+                            callback: function (value, index, values) {
+                                return value + "%";
+                            }
+                        }
+                    }]
+                }
+            }
+        });
+    }
+    function initPieChart() {
+        var ctx = document.getElementById("pie_chart").getContext('2d');
+        myChart_pie = new Chart(ctx, {
+            type: 'pie',
+            data: {
+                labels: ["Attend","Absent"],
+                datasets: [{
+                    label: 'Attendance for different subject',
+                    data: [77, 23],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)'
+
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)'
+
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            // options: {
+            //     scales: {
+            //         yAxes: [{
+            //             ticks: {
+            //                 beginAtZero: true,
+            //                 callback: function (value, index, values) {
+            //                     return value + "%";
+            //                 }
+            //             }
+            //         }]
+            //     }
+            // }
+        });
+    }
+    function initLineChart() {
+        var ctx = document.getElementById("line_chart").getContext('2d');
+        myChart_line = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: ["2014", "2015", "2016", "2017", "2018"],
+                datasets: [{
+                    label: 'Attendance for recent year',
+                    data: [77, 84, 89, 91,90],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)'
                     ],
                     borderWidth: 1
                 }]
@@ -108,9 +178,31 @@ $(function () {
                     $(this).attr('name', "");
                 }
             });
-
-
         });
+
+
+        $("#pie_Select").on("change",function(){
+            var value =$("#pie_Select").find("option:selected").val();
+            if(value==1){
+                myChart_pie.data.datasets.forEach(function (dataset) {
+                    dataset.data = [77,23];
+                })
+
+            }else if(value==2){
+                myChart_pie.data.datasets.forEach(function (dataset) {
+                    dataset.data = [75,25];
+                })
+
+            }else if(value==3){
+                myChart_pie.data.datasets.forEach(function (dataset) {
+                    dataset.data = [85,15];
+                })
+
+            }
+            myChart_pie.update();
+        });
+
+
 
         $("#yearId").mouseover(function () {
             if (!$("#yearId").hasClass('hover')) {//类hover在下面用来验证是否需要隐藏下拉，没有其他用途。
